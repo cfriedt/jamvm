bbe006b4360f2d7f67b38d9cbbc21d45c3c8b5d4
rlougher <rlougher>
2006-05-07 18:24:15 +0000

    Use new Package constructor, with extra classloader parameter.

--- lib/java/lang/VMClassLoader.java
+++ lib/java/lang/VMClassLoader.java
@@ -100,6 +100,7 @@ final class VMClassLoader
                   "GNU Classpath",
                   "GNU",
                   Configuration.CLASSPATH_VERSION,
+                  null,
                   null);
 
             definedPackages.put(packages[i], p);
