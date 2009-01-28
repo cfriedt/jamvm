41ff0742ce7997a33e172ad6e815f69019ebd6b2
rlougher <rlougher>
2009-01-28 01:40:26 +0000

    Include needed for openSolaris

--- src/thread.h
+++ src/thread.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009
  * Robert Lougher <rob@lougher.org.uk>.
  *
  * This file is part of JamVM.
@@ -20,6 +20,7 @@
  */
 
 #ifndef CREATING
+#include <alloca.h>
 #include <pthread.h>
 #include <setjmp.h>
 #include <stdlib.h>
