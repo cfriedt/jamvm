78c880d0d31c438787dbf1fd33c858dc3a4f2617
Robert Lougher <rob@jamvm.org.uk>
2011-02-14 20:49:06 +0000

    Return types smaller than an int must be handled specially
    Signed-off-by: Robert Lougher <rob@jamvm.org.uk>

--- src/os/linux/x86_64/stubs_md.c
+++ src/os/linux/x86_64/stubs_md.c
@@ -37,6 +37,25 @@ static char mapSigElement2Simple(char element) {
     }
 }
 
+static char mapRet2Simple(char element) {
+    switch(element) {
+        case 'B':
+        case 'Z':
+            return 'B';
+
+        case 'C':
+        case 'S':
+        case 'J':
+        case 'D':
+        case 'F':
+        case 'V':
+            return element;
+
+        default:
+            return 'I';
+    }
+}
+
 char *convertSig2Simple(char *sig) {
     char *simple_sig = sysMalloc(strlen(sig) + 1);
     char *simple_pntr = simple_sig;
@@ -53,7 +72,7 @@ char *convertSig2Simple(char *sig) {
     }
 
     *simple_pntr++ = ')';
-    *simple_pntr++ = mapSigElement2Simple(*++sig_pntr);
+    *simple_pntr++ = mapRet2Simple(*++sig_pntr);
     *simple_pntr++ = '\0';
 
     return sysRealloc(simple_sig, simple_pntr - simple_sig);
