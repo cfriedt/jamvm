b8e5dbad856be3b3bb0309467264053d1e2c58a9
Robert Lougher <rob@jamvm.org.uk>
2011-08-15 02:04:14 +0100

    Fix typo in definition of ACC_MIRANDA
    The VM creates methods internally for unimplemented
    interface methods (so called Miranda methods).  These
    are marked via a bit in the method access flags.
    
    This bit should have been an unused bit in the access flags
    (0x8000).  But due to a typo (a long time ago) it was defined
    as 0x0800.  This conflicts with ACC_STRICT (strictfp method).
    
    Miranda methods are not returned in getDeclaredMethods().
    The typo means strictfp methods look like Miranda methods
    and aren't returned.
    
    Signed-off-by: Robert Lougher <rob@jamvm.org.uk>

--- src/jam.h
+++ src/jam.h
@@ -302,14 +302,17 @@
 #define ACC_SYNCHRONIZED        0x0020
 #define ACC_SUPER               0x0020
 #define ACC_VOLATILE            0x0040
+#define ACC_BRIDGE              0x0040
+#define ACC_VARARGS             0x0080
 #define ACC_TRANSIENT           0x0080
 #define ACC_NATIVE              0x0100
 #define ACC_INTERFACE           0x0200
 #define ACC_ABSTRACT            0x0400
+#define ACC_STRICT              0x0800
 #define ACC_SYNTHETIC           0x1000
 #define ACC_ANNOTATION          0x2000
 #define ACC_ENUM                0x4000
-#define ACC_MIRANDA             0x0800
+#define ACC_MIRANDA             0x8000
 #define ACC_REFLECT_MASK        0xffff
 
 #define T_BOOLEAN               4
